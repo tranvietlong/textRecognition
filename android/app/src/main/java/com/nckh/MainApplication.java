3a3ce9543b4bf7d78c938e280ca29b1d1d620047
whiteBear <tranvietlong2k@gmail.com>
2023-04-14 01:38:11 +0700

    add firebase ML on Android devices

--- android/app/src/main/java/com/nckh/MainApplication.java
+++ android/app/src/main/java/com/nckh/MainApplication.java
@@ -13,6 +13,7 @@ import com.facebook.soloader.SoLoader;
 import com.nckh.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.fetchsky.RNTextDetector.RNTextDetectorPackage;
 
 public class MainApplication extends NavigationApplication {
 
@@ -25,6 +26,7 @@ public class MainApplication extends NavigationApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
+          new RNTextDetectorPackage();
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
--- src/screen/textDetector.js
+++ src/screen/textDetector.js
@@ -1,14 +1,166 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  PermissionsAndroid,
+  FlatList,
+} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import RNTextDetector from 'react-native-text-detector';
+import ImagePicker from 'react-native-image-crop-picker';
 
 const textDetector = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [text, setText] = useState(null);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [imageUri, setImageUri] = useState(null);
+
+  const openCamera = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        {
+          title: 'App Camera Permission',
+          message: 'App needs access to your camera ',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('Camera permission given');
+      } else {
+        console.log('Camera permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+    // end to require permissions android
+
+    ImagePicker.openCamera({
+      width: 1000,
+      height: 700,
+      cropping: true,
+      freeStyleCropEnabled: true,
+      useFrontCamera: true,
+    }).then(image => {
+      if (image) {
+        setImageUri(image);
+      } else {
+        console.log('we cannot access the image');
+      }
+    });
+  };
+
+  const openLibrary = () => {
+    ImagePicker.openPicker({
+      width: 1000,
+      height: 700,
+      cropping: true,
+      freeStyleCropEnabled: true,
+    }).then(image => {
+      if (image) {
+        setImageUri(image);
+      } else {
+        console.log('we cannot access the image');
+      }
+    });
+  };
+
+  // const detectText = async () => {
+  //   try {
+  //     const options = {
+  //       quality: 0.8,
+  //       base64: true,
+  //       skipProcessing: true,
+  //     };
+  //     const {uri} = await this.camera.takePictureAsync(options);
+  //     const visionResp = await RNTextDetector.detectFromUri(uri);
+  //     console.log('visionResp', visionResp);
+  //   } catch (e) {
+  //     console.warn(e);
+  //   }
+  // };
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    (async () => {
+      if (imageUri) {
+        const result = await RNTextDetector.detectFromUri(imageUri.path);
+        setText(result);
+      }
+    })();
+  }, [imageUri]);
+
+  const Item = ({title}) => <Text style={styles.item}> * {title}</Text>;
+
   return (
-    <View>
-      <Text>textDetector</Text>
-    </View>
+    <SafeAreaView style={styles.screen}>
+      <Text style={styles.title}>Text Recognition</Text>
+      <View>
+        <TouchableOpacity style={styles.button} onPress={openCamera}>
+          <Text style={styles.buttonText}>Take Photo</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={openLibrary}>
+          <Text style={styles.buttonText}>Pick a Photo</Text>
+        </TouchableOpacity>
+        {imageUri ? (
+          <Image source={{uri: imageUri.path}} style={styles.image} />
+        ) : null}
+      </View>
+      <FlatList
+        data={text}
+        renderItem={({item}) => <Item title={item} />}
+        keyExtractor={item => item}
+        style={styles.flatList}
+      />
+    </SafeAreaView>
   );
 };
 
 export default textDetector;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 35,
+    marginVertical: 20,
+  },
+  button: {
+    backgroundColor: '#47477b',
+    color: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 50,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+  },
+  image: {
+    resizeMode: 'stretch',
+    borderColor: 'pink',
+    borderWidth: 2,
+    height: 200,
+    width: 350,
+    marginTop: 30,
+    borderRadius: 10,
+  },
+  flatList: {
+    paddingVertical: 30,
+  },
+  item: {
+    padding: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});

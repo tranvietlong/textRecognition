2e9f90ff48b60e538821915894da461a606da169
whiteBear <tranvietlong2k@gmail.com>
2023-04-14 00:21:38 +0700

    add bottom tabs

---
+++ index.js
@@ -0,0 +1,80 @@
+import {Navigation} from 'react-native-navigation';
+import App from './App';
+import textDetector from './src/screen/textDetector';
+
+Navigation.registerComponent('Home', () => App);
+Navigation.registerComponent('textDetector', () => textDetector);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setDefaultOptions({
+    statusBar: {
+      backgroundColor: '#4d089a',
+    },
+    topBar: {
+      title: {
+        text: 'Research 2022',
+        color: 'white',
+      },
+      backButton: {
+        color: 'black',
+      },
+      background: {
+        color: '#4d089a',
+      },
+    },
+    bottomTab: {
+      selectedTextColor: 'red',
+    },
+  });
+
+  Navigation.setRoot({
+    root: {
+      bottomTabs: {
+        children: [
+          {
+            stack: {
+              children: [
+                {
+                  component: {
+                    name: 'Home',
+                    options: {
+                      topBar: {
+                        title: {
+                          text: 'Text Recognition',
+                        },
+                      },
+                      bottomTab: {
+                        text: 'Text Recognition',
+                      },
+                    },
+                  },
+                },
+              ],
+            },
+          },
+          {
+            stack: {
+              children: [
+                {
+                  component: {
+                    name: 'textDetector',
+                    options: {
+                      topBar: {
+                        title: {
+                          text: 'Text Detector',
+                        },
+                      },
+                      bottomTab: {
+                        text: 'Text Detector',
+                      },
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  });
+});
---
+++ src/screen/textDetector.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const textDetector = () => {
+  return (
+    <View>
+      <Text>textDetector</Text>
+    </View>
+  );
+};
+
+export default textDetector;
+
+const styles = StyleSheet.create({});
